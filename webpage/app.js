418753a488190ab7f3d6cb7283c5d4d0436ba49f
Surya Setty <siriouslysurya@gmail.com>
2025-06-06 20:01:09 -0700

    Update app.js

--- webpage/app.js
+++ webpage/app.js
@@ -190,7 +190,7 @@ const chartsDrawn = new Set();
 
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry);
+        // console.log(entry);
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
             const chartId = entry.target.querySelector("svg")?.id;
